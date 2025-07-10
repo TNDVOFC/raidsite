f2ca9fe53e8292f534ef2fa5e7b38088fff091b9
TNDVOFC <alanoliver10@gmail.com>
2025-07-09 21:26:16 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 const KEY_UNIVERSAL = "bytonymelivzmontop";
+const MENSAGEM_FIXA = "@everyone By Tony_Dev Entrem: https://discord.gg/xpXJkjDbmv";
 
 function toast(mensagem, tempo = 3000) {
   const toast = document.getElementById("toast");
@@ -29,6 +30,26 @@ function abrirRaid(key) {
 
   document.getElementById("keyInfo").innerText = key;
   document.getElementById("diasRestantes").innerText = "Ilimitado";
+}
+
+function enviarRaid() {
+  const webhook = document.getElementById("webhookRaid").value.trim();
+  const quant = parseInt(document.getElementById("quantidadeRaid").value);
+
+  if (!webhook || isNaN(quant) || quant < 1) {
+    toast("❗ Preencha todos os campos corretamente!");
+    return;
+  }
+
+  for (let i = 0; i < quant; i++) {
+    fetch(webhook, {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({ content: MENSAGEM_FIXA })
+    }).catch((err) => console.error("Erro ao enviar spam:", err));
+  }
+
+  toast("✅ Spam enviado!");
 }
 
 document.getElementById("btnGetKey").addEventListener("click", () => {
